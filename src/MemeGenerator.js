6cc322b5d008495ed0507f8393c936a8994d65b2
ebrocato <emilybrocato@outlook.com>
2020-04-21 23:03:50 -0500

    finished smart components inincluding handleSubmit method

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -7,12 +7,10 @@ class MemeGenerator extends React.Component {
     this.state ={
       topText: "",
       bottomText: "",
-      randomImg: "",
+      randomImg: "https://i.imgflip.com/1bij.jpg",
       memeData: []
     }
   }
-
-  
 
   componentDidMount = () => {
     fetch("https://api.imgflip.com/get_memes")
@@ -22,20 +20,34 @@ class MemeGenerator extends React.Component {
           this.setState({
            memeData: memes
     })})
-    
-   
+  
   }
 
-  handleChange = () => {
-    
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    // console.log('clicked')
+    //Math.ceil (ceiling) is not used when generated random number based on array length because it has a bias.
+    //it will never round to the zero indice
+    
+    const randomNum = Math.floor(Math.random() * this.state.memeData.length)
+    const genNewRandImg = this.state.memeData[randomNum].url
+    this.setState({
+      randomImg: genNewRandImg
+    })
+  }
 
   render(){
     return(
       <div>
-        <main>
-          <form>
+        
+          <form onClick={this.handleSubmit}>
             <input
               type="text"
               placeholder="Top text"
@@ -50,8 +62,14 @@ class MemeGenerator extends React.Component {
               value={this.state.bottomText}
               onChange={this.handleChange}
             />
+            <button>Generate</button>
           </form>
-        </main>
+
+          <div>
+            <h1>{this.state.topText}</h1>
+            <h1>{this.state.bottomText}</h1>
+            <img src={this.state.randomImg} alt="meme" />
+          </div>
       </div>
     )
   }
