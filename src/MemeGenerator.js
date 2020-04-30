7f6181ee64835b3f99b8c20386491a8c8f992c92
ebrocato <emilybrocato@outlook.com>
2020-04-30 12:59:34 -0500

    added new feature that clears input field after submitting. updated README.md

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -5,8 +5,11 @@ class MemeGenerator extends React.Component {
   constructor(){
     super()
     this.state ={
+      
       topText: "",
+      topTextImg: "",
       bottomText: "",
+      bottomTextImg: "",
       randomImg: "https://i.imgflip.com/1bij.jpg",
       memeData: []
     }
@@ -28,23 +31,38 @@ class MemeGenerator extends React.Component {
     this.setState({
       [name]: value
     })
+    if(name === "topText"){
+      this.setState({
+        topTextImg:value
+      })
+    }
+    if (name === "bottomText") {
+      this.setState({
+        bottomTextImg: value
+      })
+    }
   }
-
+  
+ 
   handleSubmit = (event) => {
     event.preventDefault()
+    
     // console.log('clicked')
     //Math.ceil (ceiling) is not used when generated random number based on array length because it has a bias.    
     const randomNum = Math.floor(Math.random() * this.state.memeData.length)
     const genNewRandImg = this.state.memeData[randomNum].url
     this.setState({
-      randomImg: genNewRandImg
+      randomImg: genNewRandImg,
+      topText:"",
+      bottomText:""
     })
+    
   }
 
   render(){
     return(
       <div>
-          <form className="memeForm" onSubmit={this.handleSubmit}>
+          <form className="memeForm" onSubmit={this.handleSubmit} >
             <input
               id="topText"
               type="text"
@@ -68,8 +86,8 @@ class MemeGenerator extends React.Component {
           <div className="meme">
             <img src={this.state.randomImg} alt="meme" />
             {/* Only render headings if there is text in it to ensure accessibility */}
-            {this.state.topText ? <h2 className="topText">{this.state.topText}</h2>: null } 
-            {this.state.bottomText ? <h2 className="bottomText">{this.state.bottomText}</h2> : null }
+            {this.state.topTextImg ? <h2 className="topText">{this.state.topTextImg}</h2>: null } 
+            {this.state.bottomTextImg ? <h2 className="bottomText">{this.state.bottomTextImg}</h2> : null }
           </div>
       </div>
     )
